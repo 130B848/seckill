7265e0e23e7cef1889c934f0d16159bc03fb3143
Li Haoyu <444827394@qq.com>
2017-07-04 10:56:20 +0800

    add some missing code in last commit

--- SecKill/demo.c
+++ SecKill/demo.c
@@ -21,40 +21,67 @@ struct commodityInfo {
 typedef struct commodityInfo commodity_t;
 static commodity_t commodities[MAX_NUM];
 
-void sort() {
+void swap(char *x, char *y) {
 	char t[MAX_LEN];
-	float tmp;
+	strcpy(t, x);
+	strcpy(x, y);
+	strcpy(y, t);
+}
+
+void sort() {
 	int i, j;
 	for (i = 0; i < userNum; i++)
 		for (j = i + 1; j < userNum; j++)
 			if (strcmp(users[i].id, users[j].id) > 0) {
 				// swap users[i] and users[j]
-				
-				strcpy(t, users[i].id);
-				strcpy(users[i].id, users[j].id);
-				strcpy(users[j].id, t);
-
-				strcpy(t, users[i].name);
-				strcpy(users[i].name, users[j].name);
-				strcpy(users[j].name, t);
+				swap(users[i].id, users[j].id);
+				swap(users[i].name, users[j].name);
 			}
 	for (i = 0; i < commodityNum; i++)
 		for (j = i + 1; j < commodityNum; j++)
 			if (strcmp(commodities[i].id, commodities[j].id) > 0) {
 				// swap commodities[i] and commodities[j]
+				swap(commodities[i].id, commodities[j].id);
+				swap(commodities[i].name, commodities[j].name);
 
-				strcpy(t, commodities[i].id);
-				strcpy(commodities[i].id, commodities[j].id);
-				strcpy(commodities[j].id, t);
-
-				strcpy(t, commodities[i].name);
-				strcpy(commodities[i].name, commodities[j].name);
-				strcpy(commodities[j].name, t);
-
-				tmp = commodities[i].price;
+				float tmp = commodities[i].price;
 				commodities[i].price = commodities[j].price;
 				commodities[j].price = tmp;
 			}
+}
+
+void print() {
+	int i;
+	for (i = 0; i < userNum; i++)
+		printf("%s, %s\n", users[i].id, users[i].name);
+	for (i = 0; i < commodityNum; i++)
+		printf("%s, %s, %f\n", commodities[i].id, commodities[i].name, commodities[i].price);
+}
+
+void check_sort() {
+	int i, j, flag = 0;
+	for (i = 0; i < userNum; i++)
+		for (j = i + 1; j < userNum; j++)
+			if (strcmp(users[i].id, users[j].id) > 0) {
+				flag = 1;
+				break;
+			}
+	if (flag) 
+		printf("users sort FAILED\n");
+	else
+		printf("users sort OK\n");
+
+	flag = 0;
+	for (i = 0; i < commodityNum; i++)
+		for (j = i + 1; j < commodityNum; j++)
+			if (strcmp(commodities[i].id, commodities[j].id) > 0) {
+				flag = 1;
+				break;
+			}
+	if (flag) 
+		printf("commodities sort FAILED\n");
+	else
+		printf("commodities sort OK\n");
 }
 
 int id2idx_user(char *id) {
@@ -71,20 +98,41 @@ int id2idx_user(char *id) {
 	return -1;
 }
 
-void check() {
+int id2idx_cmdt(char *id) {
+	int l = 0, r = commodityNum - 1;
+	while (l <= r) {
+		int mid = (l + r) >> 1;
+		int d = strcmp(id, commodities[mid].id);
+		if (d > 0)
+			l = mid + 1;
+		else if (d < 0)
+			r = mid - 1;
+		else return mid;
+	}
+	return -1;
+}
+
+void check_binary_search() {
 	int i;
 	for (i = 0; i < userNum; i++)
 		if (id2idx_user(users[i].id) != i) {
 			printf("fail at %d\n", i);
 			return;
 		}
-	printf("OK\n");
+	printf("users binary search OK\n");
+
+	for (i = 0; i < commodityNum; i++)
+		if (id2idx_cmdt(commodities[i].id) != i) {
+			printf("fail at %d\n", i);
+			return;
+		}
+	printf("commodities binary search OK\n");
 }
 
 int data_init() {
     FILE *fp;
 
-    if ((fp = fopen("test1000.txt", "r")) == NULL) {
+    if ((fp = fopen("input.txt", "r")) == NULL) {
         return -1;
     }
 
@@ -101,8 +149,8 @@ int data_init() {
     memset(commodities, 0, sizeof(commodity_t) * MAX_NUM);
     int number;
     for (i = 0; i < commodityNum; i++) {
-        fscanf(fp, "%36[^,],%36[^,],%d,%f\n", commodities[i].id, commodities[i].name, 
-                &number, &commodities[i].price);
+        fscanf(fp, "%36[^,],%36[^,],%f,%d\n", commodities[i].id, commodities[i].name, 
+                &commodities[i].price, &number);
     }
     
     fclose(fp);
@@ -112,5 +160,7 @@ int data_init() {
 int main() {
 	data_init();
 	sort();
-	check();
+	check_sort();
+	check_binary_search();
+	print();
 }
